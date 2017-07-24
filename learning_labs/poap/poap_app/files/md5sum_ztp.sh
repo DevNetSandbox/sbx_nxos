afb67b2a746a9c96c57155e7199cad43bb52ba83
Ajay Chenampara <ajay.chenampara@gmail.com>
2017-07-24 15:16:12 -0400

    Initial commit of poap server

---
+++ learning_labs/poap/poap_app/files/md5sum_ztp.sh
@@ -0,0 +1,2 @@
+f=ztp_script.py ; cat $f | sed '/^#md5sum/d' > $f.md5 ; sed -i \
+"s/^#md5sum=.*/#md5sum=\"$(md5sum $f.md5 | sed 's/ .*//')\"/" $f
---
+++ learning_labs/poap/poap_app/poap_app.wsgi
@@ -0,0 +1,3 @@
+import sys
+sys.path.insert(0, '/')
+from poap_server import app as application
