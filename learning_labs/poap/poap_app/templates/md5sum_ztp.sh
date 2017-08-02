2e79ea4375be5ed51857303db3caf89e8cab4678
Ajay Chenampara <ajay.chenampara@gmail.com>
2017-08-02 15:03:14 -0400

    Updated md5sum script with modified poap script file name

--- learning_labs/poap/poap_app/templates/md5sum_ztp.sh
+++ learning_labs/poap/poap_app/templates/md5sum_ztp.sh
@@ -1,2 +1,2 @@
-f=/root/sbx_nxos/learning_labs/poap/poap_app/templates/ztp_script.py ; cat $f | sed '/^#md5sum/d' > $f.md5 ; sed -i \
+f=/root/sbx_nxos/learning_labs/poap/poap_app/templates/poap_script.py ; cat $f | sed '/^#md5sum/d' > $f.md5 ; sed -i \
 "s/^#md5sum=.*/#md5sum=\"$(md5sum $f.md5 | sed 's/ .*//')\"/" $f
